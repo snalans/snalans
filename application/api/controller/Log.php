5f942c0ab250a4de88e36f96bc58af68ac41c015
xsa2864 <xsa2864@163.com>
2021-10-26 17:12:30 +0800

    优化有效值

--- application/api/controller/Log.php
+++ application/api/controller/Log.php
@@ -28,6 +28,7 @@ class Log extends Api
      * @ApiParams   (name="per_page", type="int", description="数量")
      * 
      * @ApiReturnParams   (name="serial_number", type="string", description="值来源的会员编号")
+     * @ApiReturnParams   (name="type", type="int", description="类型：1孵化，2购买蛋")
      * @ApiReturnParams   (name="number", type="string", description="数量")
      * @ApiReturnParams   (name="add_time", type="string", description="获得时间")
      */
@@ -36,7 +37,7 @@ class Log extends Api
         $page       = $this->request->get("page",1);
         $per_page   = $this->request->get("per_page",15);
         $list = Db::name("egg_valid_number_log")->alias("l")
-                ->field("u.serial_number,l.number,FROM_UNIXTIME(l.add_time,'%Y-%m-%d %H:%i') as add_time")
+                ->field("u.serial_number,l.type,l.number,FROM_UNIXTIME(l.add_time,'%Y-%m-%d %H:%i') as add_time")
                 ->join("user u","u.id=l.origin_user_id","LEFT")
                 ->where("l.user_id",$this->auth->id)
                 ->paginate($per_page);
