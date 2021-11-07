dbf3f430606374d83b99280c5f31e154ce49976f
xsa2864 <xsa2864@163.com>
2021-11-07 14:03:01 +0800

    优化

--- application/api/controller/User.php
+++ application/api/controller/User.php
@@ -97,9 +97,9 @@ class User extends Api
             $this->error(__('Invalid parameters'));
         }
 
-        // if(!captcha_check($captcha)){
-        //      $this->error("验证码错误");
-        // };
+        if(!captcha_check($captcha)){
+             $this->error("验证码错误");
+        };
 
         $ret = $this->auth->login($account, $password);
         if ($ret) {
