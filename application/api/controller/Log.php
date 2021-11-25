85e47e2b6b474a50e55d641c367b5a9f75ce8cf1
xsa2864 <xsa2864@163.com>
2021-11-25 14:25:37 +0800

    优化团队列表

--- application/api/controller/Log.php
+++ application/api/controller/Log.php
@@ -24,6 +24,7 @@ class Log extends Api
      * 获取有效值日志
      * 
      * @ApiMethod (GET)
+     * @ApiParams   (name="is_team", type="int", description="是否团队有效值 1=是 0=不是")
      * @ApiParams   (name="page", type="int", description="页码")
      * @ApiParams   (name="per_page", type="int", description="数量")
      * 
@@ -34,14 +35,30 @@ class Log extends Api
      */
     public function getValidLog()
     {
-        $page       = $this->request->get("page",1);
-        $per_page   = $this->request->get("per_page",15);
-        $list = Db::name("egg_valid_number_log")->alias("l")
-                ->field("u.serial_number,l.type,l.number,FROM_UNIXTIME(l.add_time,'%Y-%m-%d %H:%i') as add_time")
-                ->join("user u","u.id=l.origin_user_id","LEFT")
-                ->where("l.user_id",$this->auth->id)
-                ->order("l.add_time","desc")
-                ->paginate($per_page);
+        $is_team        = $this->request->get("is_team/d",0);
+        $page           = $this->request->get("page",1);
+        $per_page       = $this->request->get("per_page",15);
+
+        if($is_team == 1){
+            $wh = [];
+            $wh['c.ancestral_id'] = $this->auth->id;
+            $wh['c.level']        = ['<',4];
+            $list = Db::name("egg_valid_number_log")->alias("l")
+                    ->field("u.serial_number,l.type,l.number,FROM_UNIXTIME(l.add_time,'%Y-%m-%d %H:%i') as add_time")
+                    ->join("user u","u.id=l.origin_user_id","LEFT")
+                    ->join("membership_chain c","c.user_id=l.origin_user_id","LEFT")
+                    ->where($wh)
+                    ->order("l.add_time","desc")
+                    ->paginate($per_page);
+        }else{
+
+            $list = Db::name("egg_valid_number_log")->alias("l")
+                    ->field("u.serial_number,l.type,l.number,FROM_UNIXTIME(l.add_time,'%Y-%m-%d %H:%i') as add_time")
+                    ->join("user u","u.id=l.origin_user_id","LEFT")
+                    ->where("l.user_id",$this->auth->id)
+                    ->order("l.add_time","desc")
+                    ->paginate($per_page);
+        }
         $this->success('',$list);
     }
 
