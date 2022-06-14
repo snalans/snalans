3813f562ac7ab629c87bc9a6bb0dc9ed37d8def2
xsa2864 <xsa2864@163.com>
2022-06-15 00:40:29 +0800

    优化

--- application/api/controller/mall/Order.php
+++ application/api/controller/mall/Order.php
@@ -307,7 +307,7 @@ class Order extends Api
             $log_rs = Db::name("egg_log")->insert(['user_id'=>$result['sell_user_id'],'kind_id'=>$result['kind_id'],'type'=>1,'order_sn'=>$order_sn,'number'=>$result['total_price'],'before'=>$before,'after'=>($before+$result['total_price']),'note'=>"商城订单成交",'createtime'=>time()]);
             if($rs && $grs && $log_rs){
                 Db::commit();
-                Cache::set("change_flag_".$info['sell_user_id'],1);
+                Cache::set("change_flag_".$result['sell_user_id'],1);
                 $this->success("交易成功");
             }else{
                 Db::rollback();                 
@@ -329,7 +329,7 @@ class Order extends Api
             $data['note']   = $note;
             $rs = Db::name("mall_order")->where($wh)->update($data);
             if($rs){                
-                Cache::set("change_flag_".$info['sell_user_id'],1);
+                Cache::set("change_flag_".$result['sell_user_id'],1);
                 $this->success("申请成功");
             }else{
                 $this->error("申请失败,请重试");
