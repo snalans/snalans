86d7bdaf65d7c8596bb5294416e9a938e018c6f6
xsa2864 <xsa2864@163.com>
2022-06-17 22:59:09 +0800

    优化

--- application/api/controller/Egg.php
+++ application/api/controller/Egg.php
@@ -946,7 +946,7 @@ class Egg extends Api
             DB::rollback();
             $this->error("兑换失败");
         }
-        $this->success("兑换成功，请耐心等待打款");
+        $this->success("等待审核，审核时间为12小时在此期间如有错误请及时联系客服");
     }
 
     /**
